0e04055eb362e1b1c915b8efb1559add3c48e1b6
lilitYegh <86281502+lilitYegh@users.noreply.github.com>
2022-10-18 22:29:00 +0400

    Delete slide directory

--- slide/Command.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class Command
-{
-public:
-	virtual void commandExecution() = 0;
-	//return type can be modified
-};
--- slide/Slide.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Slide
-{
-public:
-	//virtual void addElement(int pos, ElementType type) = 0;
-	virtual void removeElement(int number) = 0;
-	virtual void showAllElements() = 0;
-};
