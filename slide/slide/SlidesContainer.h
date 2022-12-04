2c745a1454e8b4ab8db4ed8775ec490056344084
lilitYegh <86281502+lilitYegh@users.noreply.github.com>
2022-12-04 18:12:25 +0400

    Delete slide directory

--- slide/slide/SlidesContainer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class SlidesContainer
-{
-public:
-	virtual void addSlide(int pos) = 0;
-	virtual void removeSlide(int pos) = 0;
-	virtual void moveTwoSlides(int p1, int p2) = 0;
-	virtual int currentSlideNum() = 0;
-};
