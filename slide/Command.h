fcb46a91aa83f67ce65514dd1b718eb728b7d38a
lilitYegh <86281502+lilitYegh@users.noreply.github.com>
2022-12-23 00:13:13 +0400

    Delete slide directory

--- slide/Command.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-#include <string>
-class CommandHandler
-{
-public:
-	virtual void execute() = 0;
-	virtual void print() = 0;
-};
--- slide/SlideShowmanager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-class SlideShowManager
-{
-public:
-	virtual void load(std::string slideshowname) = 0;//load from file
-	virtual void save() = 0;//save in file
-};
